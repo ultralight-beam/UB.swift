dae1de440f581dbdca87ac466fa35b247dd602a5
decanus <dean.eigenmann@icloud.com>
2019-06-22 19:06:13 +0200

    docs

--- Sources/UB/Transport/Transport.swift
+++ Sources/UB/Transport/Transport.swift
@@ -3,9 +3,19 @@ import Foundation
 /// The Handler function is used to handle messages received by the transport.
 public typealias Handler = (Message) -> Void
 
+/// Transports are used to send messages between nodes using different methods, e.g. wifi direct or bluetooth.
 public protocol Transport {
 
+    /// Send implements a function to send messages between nodes using the transport.
+    ///
+    /// - Parameters:
+    ///     - message: The message to send.
     func send(message: Message);
+
+    /// Listen implements a function to receive messages being sent to a node.
+    ///
+    /// - Parameters:
+    ///     - handler: The message handler to handle received messages.
     func listen(_ handler: Handler);
 
 }
