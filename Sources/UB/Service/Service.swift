777724ede3da8c29c27b1e8becf4a703f7148be9
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-06-23 05:39:17 +0200

    enhancement/docs-updates (#5)

--- Sources/UB/Service/Service.swift
+++ Sources/UB/Service/Service.swift
@@ -4,6 +4,7 @@ import Foundation
 /// Each service does one specific thing and handles all incoming messages for that service.
 public protocol Service {
 
+    /// The service identifier, these are used to route messages to specific services.
     var type: UBID { get }
 
     /// Handle implements service specific handling for messages.
