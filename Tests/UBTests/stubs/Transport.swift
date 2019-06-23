8a0f5a91d646425b4a6ea32a572abbb86d43b737
decanus <dean.eigenmann@icloud.com>
2019-06-23 17:38:21 +0200

    conform

--- Tests/UBTests/stubs/Transport.swift
+++ Tests/UBTests/stubs/Transport.swift
@@ -3,6 +3,12 @@ import UB
 
 class Transport: UB.Transport {
 
+    var status: TransportStatus {
+        get {
+            return .off
+        }
+    }
+
     func send(message: Message) { }
 
     func listen(_ handler: Handler) { }
