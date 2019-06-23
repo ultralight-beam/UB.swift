c0cfdfadd31b34dd15b414229f39541216d2701e
decanus <dean.eigenmann@icloud.com>
2019-06-23 06:00:59 +0200

    added bluetooth transport

---
+++ Sources/UB/Transport/BluetoothTransport.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/// Implements methods for sending and receiving messages via bluetooth.
+public class BluetoothTransport: Transport {
+
+    public func send(message: Message) {
+
+    }
+
+    public func listen(_ handler: Handler) {
+
+    }
+}
