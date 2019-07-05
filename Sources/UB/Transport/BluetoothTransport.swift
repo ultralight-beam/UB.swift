dac012047ccf54764091c43ae8cfcc99eb9c3f20
decanus <dean.eigenmann@icloud.com>
2019-07-05 17:36:31 -0400

    started basic bluetooth setup

--- Sources/UB/Transport/BluetoothTransport.swift
+++ Sources/UB/Transport/BluetoothTransport.swift
@@ -1,9 +1,17 @@
 import Foundation
+import CoreBluetooth
 
 /// Implements methods for sending and receiving messages via bluetooth.
-public class BluetoothTransport: Transport {
+public class BluetoothTransport: NSObject, Transport {
+
+    fileprivate var central: CBCentralManager!
 
     public var status = TransportStatus.off
+
+    public override init() {
+        super.init()
+        central = CBCentralManager(delegate: self, queue: nil)
+    }
 
     public func send(message: Message) {
 
@@ -12,4 +20,26 @@ public class BluetoothTransport: Transport {
     public func listen(_ handler: Handler) {
 
     }
+}
+
+extension BluetoothTransport: CBCentralManagerDelegate {
+
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print(central.state)
+//        switch central.state {
+//        case .unknown:
+//            <#code#>
+//        case .resetting:
+//            <#code#>
+//        case .unsupported:
+//            <#code#>
+//        case .unauthorized:
+//            <#code#>
+//        case .poweredOff:
+//            <#code#>
+//        case .poweredOn:
+//            <#code#>
+//        }
+    }
+
 }
