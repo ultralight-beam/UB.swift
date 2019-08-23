d2c7fcef74593df4e9741498c55fb2bea7e7d755
Eric Tu <tu.eric@hotmail.com>
2019-08-24 01:30:34 +0200

    nothing works

--- Sources/Node/main.swift
+++ Sources/Node/main.swift
@@ -1,1 +1,62 @@
-import Foundation
+//import CoreBluetooth
+//
+//class Delegates:
+//    NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+//
+//    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+//        if central.state == CBManagerState.poweredOn {
+//            central.scanForPeripherals(withServices: nil, options: nil)
+//        } else {
+//            print("Bluetooth not available.a")
+//        }
+//    }
+//}
+//
+//let a = Delegates();
+//let manager = CBCentralManager(delegate: a, queue: nil)
+//
+//
+//if manager.state == CBManagerState.poweredOn {
+//    manager.scanForPeripherals(withServices: nil, options: nil)
+//} else {
+//    print("Bluetooth not available.b")
+//}
+
+import IOBluetooth
+
+class BluetoothDevices {
+    
+    let bluetoothDeviceInquiryDelegate = BluetoothDeviceInquiryDelegate()
+    let inquery = IOBluetoothDeviceInquiry(delegate: bluetoothDeviceInquiryDelegate);
+    
+    func pairedDevices() {
+        print("Bluetooth devices:")
+        guard let devices = IOBluetoothDevice.pairedDevices() else {
+            print("No devices")
+            return
+        }
+        for item in devices {
+            if let device = item as? IOBluetoothDevice {
+                print("Name: \(device.name)")
+                print("Paired?: \(device.isPaired())")
+                print("Connected?: \(device.isConnected())")
+            }
+        }
+    }
+    func scanForDevices() -> IOReturn {
+         return inquery.start()
+    }
+    
+    func getDevices() -> [Any] {
+         return inquery.foundDevices();
+    }
+}
+
+var bt = BluetoothDevices()
+//bt.pairedDevices()
+
+print("SCAN \(bt.scanForDevices())")
+
+print("devs \(bt.getDevices())")
+
+RunLoop.current.run()
