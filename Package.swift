23788b82f8dcb358ca875e64136669533bd9aa85
decanus <dean.eigenmann@icloud.com>
2019-08-29 00:08:09 +0200

    updated

--- Package.swift
+++ Package.swift
@@ -6,23 +6,17 @@ import PackageDescription
 let package = Package(
     name: "UB",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
+       .library(
             name: "UB",
             targets: ["UB"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+       .target(
             name: "UB",
             dependencies: []),
         .testTarget(
             name: "UBTests",
-            dependencies: ["UB"]),
+            dependencies: ["UB"])
     ]
 )
