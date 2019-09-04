38b0e69f718fb70de3ad2e42578b6d8fea6424e2
decanus <dean.eigenmann@icloud.com>
2019-09-04 16:45:16 -0400

    documented

--- Sources/UB/Peer.swift
+++ Sources/UB/Peer.swift
@@ -2,10 +2,19 @@ import Foundation
 
 // @todo clean this up properly, currently very rough for testing purposes.
 
+/// Represents a peer for a specific transport.
 public class Peer {
+    /// The peers `id`.
     public let id: Addr
+
+    /// The services a peer knows.
     public let services: [UBID]
 
+    /// The init function for a peer.
+    ///
+    /// - Parameters
+    ///     - id: The peers `id`
+    ///     - services: The services a peer can knows.
     init(id: Addr, services: [UBID]) {
         self.id = id
         self.services = services
