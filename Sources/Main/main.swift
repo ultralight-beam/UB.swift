0b171d8fbe498dcb5a43a7c8786c76fd0b87b357
decanus <dean.eigenmann@icloud.com>
2019-09-05 23:12:36 -0400

    fixed

--- Sources/Main/main.swift
+++ Sources/Main/main.swift
@@ -8,19 +8,14 @@ let message = Message(
     recipient: Addr(repeating: 4, count: 4),
     from: Addr(repeating: 2, count: 3),
     origin: Addr(repeating: 2, count: 3),
-    message: Data(repeating: 7, count: 3)
+    message: Data(repeating: 8, count: 3)
 )
 
-if #available(OSX 10.12, *) {
-    let timer = Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
-
-        print(UBBT.peripherals.count)
-        if UBBT.peripherals.count == 1 {
-            UBBT.send(message: message, to: Array(UBBT.peripherals.keys)[0])
-        }
+let timer = Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
+    print(UBBT.peripherals.count)
+    if UBBT.peripherals.count == 1 {
+        UBBT.send(message: message, to: Array(UBBT.peripherals.keys)[0])
     }
-} else {
-    // Fallback on earlier versions
 }
 
 RunLoop.current.run()
