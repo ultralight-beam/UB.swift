5c9bf8446845d25a6fdb63c6828ff2f36bc0b7b8
decanus <dean.eigenmann@icloud.com>
2019-09-11 14:13:14 -0400

    removed peer

--- Sources/UB/Transports/Stream/StreamClient.swift
+++ Sources/UB/Transports/Stream/StreamClient.swift
@@ -2,53 +2,64 @@ import Foundation
 
 /// The StreamClient implements generic stream handling for Ultralight Beam transports.
 public class StreamClient: NSObject {
-    
     // @TODO: We need to figure out how the dependants figure out which address or peer data came from.
-    
+
     private let input: InputStream
     private let output: OutputStream
-    
+
     /// The delegate for the StreamClient.
     weak var delegate: StreamClientDelegate?
-    
-    /// Initializes a StreamClient with the peer address and the input and output streams.
+
+    /// Initializes a StreamClient with the input and output streams.
     ///
     /// - Parameters:
     ///     - input: The input stream.
     ///     - output: The output stream.
-    public init(peer: Addr, input: InputStream, output: OutputStream) {
+    public init(input: InputStream, output: OutputStream) {
         self.input = input
         self.output = output
-        
+
         super.init()
-        
+
         self.input.delegate = self
         self.output.delegate = self
     }
-    
+
     /// Writes the contents of provided data to the receiver.
     ///
     /// - Parameters:
     ///     - data: Data to write
     public func write(_ data: Data) {
         let bytes = NSMutableData()
-        
+
         var length = UInt32(data.count).bigEndian
         bytes.append(&length, length: 4)
-        
+
         bytes.append(data)
-        
+
         output.write([UInt8](bytes), maxLength: bytes.count)
     }
 }
 
 /// :nodoc:
 extension StreamClient: StreamDelegate {
-    public func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
-        
-    }
+    public func stream(_: Stream, handle _: Stream.Event) {}
 
     fileprivate func read() {
-        
+        let length = read(4).withUnsafeBytes {
+            $0.load(as: UInt32.self)
+        }
+
+        delegate?.client(self, didReceiveData: read(Int(length.bigEndian)))
+    }
+
+    private func read(_ length: Int) -> Data {
+        if length == 0 {
+            return Data()
+        }
+
+        var buffer = [UInt8](repeating: 0, count: length)
+        input.read(&buffer, maxLength: length)
+        return Data(buffer)
     }
 }
