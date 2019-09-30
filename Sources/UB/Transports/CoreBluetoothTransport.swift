6503f451200d2458c3b7864baedab4e4c21bef95
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-09-30 23:59:55 +0200

    fix/duplicated-peer (#67)
    * adds a check to avoid peer duplication
    
    * added another check
    
    * removed
    
    * readd
    
    * removed
    
    * additional

--- Sources/UB/Transports/CoreBluetoothTransport.swift
+++ Sources/UB/Transports/CoreBluetoothTransport.swift
@@ -71,6 +71,11 @@ public class CoreBluetoothTransport: NSObject {
         }
 
         centrals[id] = central
+
+        if peers.filter({ $0.id == id }).count != 0 {
+            return
+        }
+
         peers.append(Peer(id: id, services: [UBID]()))
     }
 }
@@ -201,6 +206,11 @@ extension CoreBluetoothTransport: CBPeripheralDelegate {
             peripherals[id] = (peripheral, char)
             peripherals[id]?.peripheral.setNotifyValue(true, for: char)
             // @todo we may need to do some handshake to obtain services from a peer.
+
+            if peers.filter({ $0.id == id }).count != 0 {
+                return
+            }
+
             peers.append(Peer(id: id, services: [UBID]()))
         }
     }
