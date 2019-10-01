fa6a78b8c438ffff44b9abf3f5f5032da4526749
decanus <dean.eigenmann@icloud.com>
2019-10-01 20:30:55 +0200

    renamed

--- Sources/UB/Service.swift
+++ Sources/UB/Service.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// Services are responsible for handling messages received from an Ultralight Beam node.
 public protocol Service: NodeDelegate {
     /// The unique service identifier.
     var identifier: UBID { get }
