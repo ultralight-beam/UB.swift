61bdfe6062a847d1d33f520bd3645c2bb672ccad
decanus <7621705+decanus@users.noreply.github.com>
2020-05-18 13:34:08 +0200

    using min

--- Sources/UB/Extensions/Array.swift
+++ Sources/UB/Extensions/Array.swift
@@ -2,17 +2,8 @@ import Foundation
 
 extension Array where Element == Addr {
     func closest(to: Addr) -> Addr? {
-        var distance = 0
-        var addr: Addr?
-
-        forEach { peer in
-            let dist = peer.distance(to: to)
-            if distance > dist {
-                distance = dist
-                addr = peer
-            }
+        return self.min { a, b in
+            a.distance(to: to) < b.distance(to: to)
         }
-
-        return addr
     }
 }
